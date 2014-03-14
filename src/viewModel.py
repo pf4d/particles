2311f07b3e14a42845b5e190518f4a3cbae2dc60
josh-bartz <jbartzy@gmail.com>
2014-03-14 14:33:59 -0500

    Ship in viewModel is now being moved using the particle properties.

--- src/viewModel.py
+++ src/viewModel.py
@@ -3,15 +3,60 @@ from pygame.locals import *
 from pygame.constants import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from particles import *
+import numpy
+from numpy import *
+from random import randint
 
 #import obj loader
 from objLoader import *
 
 pygame.init()
+dt = .01
+radiusDiv = 1
+SLICES = 30
+STACKS = 30
 viewport = (800,600)
 hx = viewport[0]/2
 hy = viewport[1]/2
 srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
+L = 10.0
+px = 0
+py = 2*L
+pz = 0
+pvx = 0
+pvy = 0
+pvz = 0
+pr = 1
+f = GranularMaterialForce(k=1.5, g=.1, gamma=.1)
+p = Particles(L, f, periodicY=0, periodicZ=1, periodicX=1)
+integrate = VerletIntegrator(dt)
+#p.addParticle(px,py,pz,pvx,pvy,pvz,pr,0,0,0,0,0,0)
+
+def initParticles():
+    #particle = p.addParticle(0,py,0,pvx,pvy,pvz,pr,0,0,0,0,0,0)
+    for i in range(0,10):
+        spx = randint(1,40)
+        #spy = randint(1,40)
+        #spz = randint(1,40)
+        #p.addParticle(spx,py,spz,pvx,pvy,pvz,pr,0,0,0,0,0,0)
+
+def drawParticles():
+    glTranslate(p.x[0],p.y[0],p.z[0])
+    glutSolidSphere(p.r[0]/radiusDiv, SLICES, STACKS)
+    for i in range(0,10):
+        glTranslate(randint(1,40),py,randint(1,40))
+        glutSolidSphere(p.r[0]/radiusDiv, SLICES, STACKS)
+
+
+
+#  def addParticle(self, x, y, z, vx, vy, vz, r,
+#                  thetax, thetay, thetaz, 
+#                  omegax, omegay, omegaz): 
+
+
+#particle = p.addParticle(px,py,pz,pvx,pvy,pvz,pr,0,0,0,0,0,0)
 
 glLightfv(GL_LIGHT0, GL_POSITION, (-40, 200, 100, 0.0))
 glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
@@ -21,6 +66,7 @@ glEnable(GL_LIGHTING)
 glEnable(GL_COLOR_MATERIAL)
 glEnable(GL_DEPTH_TEST)
 glShadeModel(GL_SMOOTH)
+glutInit()
 
 obj = OBJ(sys.argv[1], swapyz=True)
 
@@ -37,6 +83,10 @@ rx, ry = (0,0)
 tx, ty = (0,0)
 zpos = 5
 rotate = move = False
+
+p.addParticle(px,py,pz,pvx,pvy,pvz,pr,0,0,0,0,0,0)
+
+#initParticles()
 while 1:
     clock.tick(30)
     for e in pygame.event.get():
@@ -44,6 +94,24 @@ while 1:
             sys.exit()
         elif e.type == KEYDOWN and e.key == K_ESCAPE:
             sys.exit()
+        elif e.type == KEYDOWN and e.key == K_UP:
+            #paz -= .2
+            p.az[0] -= .2
+        elif e.type == KEYDOWN and e.key == K_DOWN:
+            #paz += .2
+            p.az[0] += .2
+        elif e.type == KEYDOWN and e.key == K_a:
+            pvx -= .2
+            p.ax[0] -= .2
+        elif e.type == KEYDOWN and e.key == K_d:
+            pvx += .2
+            p.ax[0] += .2
+        elif e.type == KEYDOWN and e.key == K_w:
+            pvy += .2
+            p.ay[0] += .2
+        elif e.type == KEYDOWN and e.key == K_s:
+            pvy -= .2
+            p.ay[0] -= .2
         elif e.type == MOUSEBUTTONDOWN:
             if e.button == 4: zpos = max(1, zpos-1)
             elif e.button == 5: zpos += 1
@@ -61,13 +129,54 @@ while 1:
                 tx += i
                 ty -= j
 
+    integrate(f,p)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
 
+    #px += pvx
+    #py += pvy
+    #pz += pvz
+
+    #p.x[0] += pvx
+    #p.y[0] += pvy
+    #p.z[0] += pvz
+    particlePosition = numpy.array([p.x[0],p.y[0],p.z[0]])
+    cameraDist = particlePosition + numpy.array([20*cos(pvz),10,20*sin(pvz)])
+    cameraDist = particlePosition + numpy.array([0,3,-10])
+    cameraTarget = particlePosition
+    cameraUp = numpy.array([0,1,0])
+
+    print("x: %f  y: %f  z: %f" % (p.x[0],p.y[0],p.z[0]))
+
     # render object
-    glTranslate(tx/20., ty/20., - zpos)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    #gluLookAt(cameraDist, cameraTarget, cameraUp)
+    gluLookAt(cameraDist[0],cameraDist[1],cameraDist[2],cameraTarget[0],cameraTarget[1],cameraTarget[2],cameraUp[0],cameraUp[1],cameraUp[2])    #glTranslate(tx/20., ty/20., - zpos)
+
+
+    glTranslate(p.x[0],p.y[0],p.z[0])
     glRotate(ry, 1, 0, 0)
     glRotate(rx, 0, 1, 0)
     glCallList(obj.gl_list)
+    #glColor(0,0,1)
 
+    #glMatrixMode(GL_MODELVIEW)
+    #glLoadIdentity()
+    #gluLookAt(cameraDist, cameraTarget, cameraUp)
+    #gluLookAt(cameraDist[0],cameraDist[1],cameraDist[2],cameraTarget[0],cameraTarget[1],cameraTarget[2],cameraUp[0],cameraUp[1],cameraUp[2])
+    #glPushMatrix()
+    #glTranslate(0,0,0)
+    #glTranslate(p.x[0],p.y[0],p.z[0])
+    #glColor(1,0,0)
+    #p.addParticle(0,py,0,pvx,pvy,pvz,pr,0,0,0,0,0,0)
+    #drawParticles()
+    #glutSolidSphere(p.r[0]/radiusDiv, SLICES, STACKS)
+    #print("z: %f" % p.z[0])
+
+    #for i in range(0,20):
+        #glTranslate(p.x[0]+1,p.y[0],i)
+        #glutSolidSphere(p.r[0]/radiusDiv, SLICES, STACKS)
     pygame.display.flip()
+
+
