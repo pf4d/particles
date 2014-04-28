1e5f0f02681e623985b37f91be00dd152d2571de
Evan Cummings <evan.cummings@umontana.edu>
2014-04-28 17:36:20 -0600

    found error

--- src/particles.py
+++ src/particles.py
@@ -124,13 +124,13 @@ class GranularMaterialForce(object):
     kappa            = self.f * p.r
    
     # project onto components, sum all angular forces on each particle
-    p.alphax = + sum(-(k*taux + f*epix) / I + kappa*dox, axis=1) \
+    p.alphax = + sum((k*taux + f*epix) / I + kappa*dox, axis=1) \
                - g*p.omegax \
                + ctx
-    p.alphay = + sum(-(k*tauy + f*epiy) / I + kappa*doy, axis=1) \
+    p.alphay = + sum((k*tauy + f*epiy) / I + kappa*doy, axis=1) \
                - g*p.omegay \
                + cty
-    p.alphaz = + sum(-(k*tauz + f*epiz) / I + kappa*doz, axis=1) \
+    p.alphaz = + sum((k*tauz + f*epiz) / I + kappa*doz, axis=1) \
                - g*p.omegaz \
                + ctz
 
@@ -180,9 +180,9 @@ class VerletIntegrator(object):
     p.z = p.z + p.vz*dt + 0.5*p.az*dt**2
 
     # angular roation update :
-    p.thetax = -(p.omegax*dt + 0.5*p.alphax*dt**2)
-    p.thetay = -(p.omegay*dt + 0.5*p.alphay*dt**2)
-    p.thetaz = -(p.omegaz*dt + 0.5*p.alphaz*dt**2)
+    p.thetax = p.omegax*dt + 0.5*p.alphax*dt**2
+    p.thetay = p.omegay*dt + 0.5*p.alphay*dt**2
+    p.thetaz = p.omegaz*dt + 0.5*p.alphaz*dt**2
     p.update_theta()
 
     # Update periodic BC
